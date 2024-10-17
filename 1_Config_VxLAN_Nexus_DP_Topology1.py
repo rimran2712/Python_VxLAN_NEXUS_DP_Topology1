2a29de8689b2d15c512eede136e6c633717fb2ae
Imran <rimran84@yahoo.com>
2024-10-17 00:06:56 -0500

    added VxLAN DP

--- 1_Config_VxLAN_Nexus_DP_Topology1.py
+++ 1_Config_VxLAN_Nexus_DP_Topology1.py
@@ -17,14 +17,18 @@ this program will configure VxLAN with Data Plane Address Learning (Flood & Lear
 This Program will configure/automate only Spine/Leaf switches, vIOS-RTR will be configured as manually
 Prerequisite: VAR Files and J2 templates 
 
+DP Learning is also called Flood  & Learn
+DP only support to bridge L2 VNI
+DP does not support Inter VNI or Integrated Routing Bridging, for Inter VNI we need Router
+
 Variable Files: load variable from VARS(variable files), VARS are define for each host
 '''
-def config_device_ip_j2_template(task):
-    ip_cfg_template = task.run (task=template_file, template=f"config_dev_ip.j2", path=f"J2_Templates/{task.host.platform}")
-    task.host['dev_ip_cfg'] = ip_cfg_template.result
-    dev_ip_cfg_rendered = task.host['dev_ip_cfg']
-    dev_ip_config = dev_ip_cfg_rendered.splitlines()
-    task.run (task=send_configs, configs=dev_ip_config)
+def config_device_interfaces_j2_template(task):
+    int_cfg_template = task.run (task=template_file, template=f"config_dev_int.j2", path=f"J2_Templates/{task.host.platform}")
+    task.host['dev_int_cfg'] = int_cfg_template.result
+    dev_int_cfg_rendered = task.host['dev_int_cfg']
+    dev_int_config = dev_int_cfg_rendered.splitlines()
+    task.run (task=send_configs, configs=dev_int_config)
 
 def config_vxlan_basic_j2_template(task):
     vxlan_basic_cfg_template = task.run (task=template_file, template=f"vxlan_basic.j2", path=f"J2_Templates/{task.host.platform}")
@@ -49,10 +53,15 @@ def config_iBGP_j2_template(task):
 
 def config_VxLAN_Nexus_DP (task):
     # First of all we need to load variables (vars) for hosts using load_yaml, 
-    # it will return yaml data in dictionary form
+    
+    # it will return yaml data and store in dictionary form and embend into dic key for each specific device 
     dev_data = task.run (task=load_yaml, file=f"./Hosts_VARS/{task.host.platform}/{task.host}.yaml")
     task.host['dev_vars'] = dev_data.result
 
+    # it will return yaml data and store in dictionary form, 
+    # These are commond variables for all devices and used to generate commond configurations for all devices
+    common_data = task.run (task=load_yaml, file=f"./Hosts_VARS/{task.host.platform}/common_vars.yaml")
+    task.host['common_vars'] = common_data.result
     #Now vars are loaded, Lets configure Devices, in our lab we will configuration 
 
     # Enabled all required featured for VxLAN, Underlay OSPF process, Multicast using j2 template
@@ -60,8 +69,8 @@ def config_VxLAN_Nexus_DP (task):
     config_vxlan_basic_j2_template(task)
 
     
-    # IP addresses configuration using j2 template
-    #config_device_ip_j2_template(task)
+    # Interface level configuration (IP add, OSPF, pim etc.using j2 template
+    config_device_interfaces_j2_template(task)
 
     
     # iBGP required TCP/IP reachability to neighbors so we need to configure any IGP
